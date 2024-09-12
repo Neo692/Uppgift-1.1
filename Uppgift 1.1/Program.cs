283d35a379063fae81cb6b8fb7188b9856f2e43f
nehy06001 <nehy06001@E-PW05CGQV>
2024-09-12 14:28:14 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;//detta kommand är basen för mitt program
+
+namespace Uppgift_1//detta kommand sätter namn för mitt program
+{
+    class Program//denna kommand sätter programmet
+    {
+        static void Main(string[]args)//detta kommand gör så att raderna skrivs ut efter varann
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");//detta kommand gör så att programmet skriver ut texten
+            Console.WriteLine("Du har lärt dig att skriva ut tecken som \" och \\");//detta kommand gör så att programmet skriver ut texten
+        }
+    }
+}
